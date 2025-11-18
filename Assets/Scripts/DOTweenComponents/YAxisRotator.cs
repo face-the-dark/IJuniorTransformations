fe7c1e2f03a81b95d85a033271cb84a4748793e4
Nikolai <ne360cosmos@gmail.com>
2025-11-18 08:50:17 +0300

    Выполнено задание

---
+++ Assets/Scripts/DOTweenComponents/YAxisRotator.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace DOTweenComponents
+{
+    public class YAxisRotator : Tweener
+    {
+        protected override void Tween() => 
+            RotateOnYAxis();
+
+        private void RotateOnYAxis() =>
+            transform.DORotate(Vector3.up, StartSpeed)
+                .SetSpeedBased(true)
+                .SetLoops(LoopsCount, LoopType.Incremental)
+                .SetEase(Ease.Linear);
+    }
+}
